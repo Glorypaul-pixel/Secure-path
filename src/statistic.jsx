49c5060a7ded9546c2ec0955332dfae2b6486714
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2024-04-29 11:24:04 +0100

    feat: added responses

--- src/statistic.jsx
+++ src/statistic.jsx
@@ -9,9 +9,9 @@ function Statistic() {
     const PAGE_SIZE = 20;
     const newReports = await fetch(
       "https://security-backend-12f1.onrender.com/v1/complaint/all"
-    );
+    ).then((res) => res.json());
     if (!newReports) return;
-    if (newReports?.history?.length < PAGE_SIZE) {
+    if (newReports?.data?.length < PAGE_SIZE) {
       setNoMoreFetch(true);
     }
     setReports((prev) =>
@@ -45,7 +45,20 @@ function Statistic() {
   } else {
     // add more content here
     content = (
-      <div className="flex flex-col space-y-6">
+      <div className="flex flex-col space-y-6 w-full">
+        <div>
+
+            <h2 className="text-2xl font-medium">Complaints</h2>
+        </div>
+        <div
+            className="flex flex-row justify-between items-center"
+          >
+            <div className="flex flex-row items-center">
+              <span className="text-2xl font-bold">Report ID</span>
+              <span className="text-xl font-medium ml-4">Full name of reporter</span>
+            </div>
+              <span className="text-xl font-medium ml-4">Location of report</span>
+          </div>
         {reports?.map((val, index) => (
           <div
             key={index}
@@ -53,7 +66,7 @@ function Statistic() {
           >
             <div className="flex flex-row items-center">
               <span className="text-2xl font-bold">{index + 1}</span>
-              <span className="text-xl font-medium ml-4">{val.full_name}</span>
+              <span className="text-xl font-medium ml-4">{val?.reporter?.full_name}</span>
             </div>
             <span className="text-xl font-medium">{val.location}</span>
           </div>
@@ -67,7 +80,7 @@ function Statistic() {
       </div>
     );
   }
-  return <div>{content}</div>;
+  return <div className="w-3/4 text-white flex flex-col items-center">{content}</div>;
 }
 
 export default Statistic;
